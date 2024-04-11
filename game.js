577d41487bc7544cabbd5ed170c11c3f0068f1f0
darwind <huytton119@gmail.com>
2024-04-11 15:56:22 +0700

    update factory for players

--- game.js
+++ game.js
@@ -1,9 +1,43 @@
 const Gameboard = (() => {
-    let gameboard =
-    
+    let gameboard = ["", "", "", "", "", "", "", "", ""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id=square-${index}>${square}</div>`
+        })
+    }
+    document.querySelector("#gameboard").innerHTML = boardHTML;
+
+    return{
+        render,
+    }
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Game = (() =>{
+        let players = [];
+        let currentPlayerIndex;
+        let gameOver;
+
+        const start = () => {
+            players = [
+                createPlayer(document.querySelector("#player1").value, "X"),
+                createPlayer(document.querySelector("#player2").value, "O")
+            ]
+            currentPlayerIndex = 0;
+            gameOver = 0;
+            Gameboard.render();
+        }
 })();
 
 const startButton = document.querySelector("#start-btn");
 startButton.addEventListener("click",() => {
-    // Game.start();
+    Game.start();
 });
