7a48fb87bc4220af4d160963f09e1a6b10dd361c
darwind <huytton119@gmail.com>
2024-04-11 12:29:57 +0700

    update HTML and add start-btn function in game.js

---
+++ game.js
@@ -0,0 +1,9 @@
+const Gameboard = (() => {
+    let gameboard =
+    
+})();
+
+const startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click",() => {
+    // Game.start();
+});
